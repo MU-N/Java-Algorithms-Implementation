56f22d8b8ac4d51bc68e72d91b3c012e0ba66129
Muhammed  Nasser <39590845+MU-N@users.noreply.github.com>
2023-02-28 22:30:04 +0200

    frist commit

---
+++ src/main/java/com/mycompany/java/algorithms/implementation/JavaAlgorithmsImplementation.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java.algorithms.implementation;
+
+/**
+ *
+ * @author Nasser
+ */
+public class JavaAlgorithmsImplementation {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
